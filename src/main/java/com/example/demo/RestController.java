0423c89bce8387732f636a93a61ed88597c09210
kunal <purikunal22@gmail.com>
2019-12-25 00:18:38 +0530

    Initial Commit

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Date;
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("kube")
 public class RestController {
 
     @GetMapping
